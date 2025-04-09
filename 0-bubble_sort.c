1d9f7b2cda84a777589da93739104ea4706eb730
Theo-D <theodessaigne@hotmail.fr>
2025-04-09 13:18:50 +0200

    Added 0-bubble_sort.c that contains the definition for a function that sorts an array of integers in ascending order using the Bubble sort algorithm. Added 0-O that contains O notations for the bubble sort. Updated sorth.h to include stddef.h. Updated README.md with a descirption for 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp = 0;
+
+	if (size >= 2)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - 1; j++)
+				if (array[j] > array[j + 1])
+				{
+					temp =  array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+
+					print_array(array, size);
+				}
+		}
+	}	
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+0-bubble_sort.c - Contains definition for a function that sorts an array of integers in ascending order using the Bubble sort algorithm.
+
+
