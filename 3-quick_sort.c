b07e92e4f147712e8e5e666cedd3e8b128b6687b
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-10 20:33:37 +0200

    Created new function to be called with total size of array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,31 @@
 #include "sort.h"
 
+void quick_sort_recursive(int *array, size_t size, int *start, size_t total);
+
 /**
  * quick_sort - sorts an array in ascending order
  * @array: array to sort
  * @size: size of array
  */
 void quick_sort(int *array, size_t size)
+{
+	int total = size;
+	int *start = &array[0];
+
+	if (size > 1)
+	{
+		quick_sort_recursive(&array[0], size, start, total);
+	}
+}
+
+/**
+ * quick_sort_recursive - sorts an array in ascending order
+ * @array: array to sort
+ * @size: number of element to sort in array
+ * @start: beginning of the whole array
+ * @total: total size of the array
+ */
+void quick_sort_recursive(int *array, size_t size, int *start, size_t total)
 {
 	int pivot = size - 1;
 	int i = -1, j = 0;
@@ -25,7 +45,7 @@ void quick_sort(int *array, size_t size)
 				array[j] = array[i];
 				array[i] = temp;
 
-				print_array(&array[0], size);
+				print_array(&start[0], total);
 			}
 
 			j++;
@@ -38,10 +58,10 @@ void quick_sort(int *array, size_t size)
 		array[pivot] = array[i];
 		array[i] = temp;
 
-		print_array(&array[0], size);
+		print_array(&start[0], total);
 
 		/* Calling function recursively on left and right sections */
-		quick_sort(&array[0], i);
-		quick_sort(&array[i + 1], size - (i + 1));
+		quick_sort_recursive(&array[0], i, &start[0], total);
+		quick_sort_recursive(&array[i + 1], size - (i + 1), &start[0], total);
 	}
 }
