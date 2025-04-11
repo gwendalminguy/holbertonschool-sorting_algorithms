e4f32d879af9c5afde79c83be8778ac086d87d1a
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-11 07:03:59 +0200

    Created function sorting an array in ascending order using shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * shell_sort - sorts an array in ascending order
+ *		using shell sorting algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1, temp, swap, i;
+
+	/* Finding first gap to use from Knuth sequence */
+	while (gap * 3 + 1 < size)
+	{
+		gap = gap * 3 + 1;
+	}
+
+	/* Repeating with each gap from largest to 1 */
+	while (gap >= 1)
+	{
+		swap = 1;
+
+		/* Repeating until no values are swapped */
+		while (swap == 1)
+		{
+			swap = 0;
+			i = size - gap - 1;
+
+			/* Repeating on each value of array */
+			while (i >= 0)
+			{
+				/* Swapping values if unordered */
+				if (array[i] > array[i + gap])
+				{
+					swap = 1;
+					temp = array[i + gap];
+					array[i + gap] = array[i];
+					array[i] = temp;
+				}
+
+				i--;
+			}
+		}
+
+		/* Updating gap value */
+		gap = (gap - 1) / 3;
+
+		/* Printing the array */
+		print_array(array, size);
+	}
+}
