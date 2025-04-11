ff725abbcfb2db1901c1689fe02325198daace90
gwendalminguy <gwendalminguy@outlook.fr>
2025-04-11 07:11:01 +0200

    Corrected case where size is 1

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,43 +12,41 @@ void shell_sort(int *array, size_t size)
 	size_t gap = 1;
 	int temp, swap, i;
 
-	/* Finding first gap to use from Knuth sequence */
-	while (gap * 3 + 1 < size)
+	if (size > 1)
 	{
-		gap = gap * 3 + 1;
-	}
-
-	/* Repeating with each gap from largest to 1 */
-	while (gap >= 1)
-	{
-		swap = 1;
-
-		/* Repeating until no values are swapped */
-		while (swap == 1)
+		/* Finding first gap to use from Knuth sequence */
+		while (gap * 3 + 1 < size)
 		{
-			swap = 0;
-			i = size - gap - 1;
-
-			/* Repeating on each value of array */
-			while (i >= 0)
-			{
-				/* Swapping values if unordered */
-				if (array[i] > array[i + gap])
-				{
-					swap = 1;
-					temp = array[i + gap];
-					array[i + gap] = array[i];
-					array[i] = temp;
-				}
-
-				i--;
-			}
+			gap = gap * 3 + 1;
 		}
+		/* Repeating with each gap from largest to 1 */
+		while (gap >= 1)
+		{
+			swap = 1;
+			/* Repeating until no values are swapped */
+			while (swap == 1)
+			{
+				swap = 0;
+				i = size - gap - 1;
+				/* Repeating on each value of array */
+				while (i >= 0)
+				{
+					/* Swapping values if unordered */
+					if (array[i] > array[i + gap])
+					{
+						swap = 1;
+						temp = array[i + gap];
+						array[i + gap] = array[i];
+						array[i] = temp;
+					}
 
-		/* Updating gap value */
-		gap = (gap - 1) / 3;
-
-		/* Printing the array */
-		print_array(array, size);
+					i--;
+				}
+			}
+			/* Updating gap value */
+			gap = (gap - 1) / 3;
+			/* Printing the array */
+			print_array(array, size);
+		}
 	}
 }
